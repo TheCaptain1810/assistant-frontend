39e152839e128110431c89e42ed7d686841f1c21
Pranav Pise <160746099+TheCaptain1810@users.noreply.github.com>
2024-10-26 18:24:15 +0530

    ...3

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -120,7 +120,7 @@ export default function Chatbot() {
           { type: 'user', content: commandToSend }
         ]);
 
-        const res = await axios.post('http://127.0.0.1:10000', { command: commandToSend });
+        const res = await axios.post('https://python-backend-capstone-1.onrender.com/', { command: commandToSend });
         const newResponse = res.data.response;
         
         setIsTyping(true);
